2c03f7000a6db5597524d8fb90e3b90329f54db4
amit3200 <tusharamit@yahoo.com>
2024-06-20 02:22:42 +0530

    Percy Playwright Dotnet Example

---
+++ web/PercyAfterTest.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Newtonsoft.Json;
+using NUnit.Framework;
+using PercyIO.Playwright;
+
+namespace PercyWeb
+{
+    [TestFixture]
+    [Category("web-percy-after-test")]
+    public class PercyAfterTest
+    {
+        private IBrowser browser;
+        private IBrowserContext context;
+        private IPage page;
+        public PercyAfterTest() : base() { }
+
+        [SetUp]
+        public async Task Init()
+        {
+            try
+            {
+                var playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+                browser = await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions { Headless = false });
+                page = await browser.NewPageAsync();
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [Test]
+        public async Task SearchBstackDemo()
+        {
+            try
+            {
+                // Navigate to the page
+                await page.SetViewportSizeAsync(1280, 1024);
+                await page.GotoAsync("https://bstackdemo.com/");
+
+                // Check the title
+                var pageTitle = await page.TitleAsync();
+                Assert.IsTrue(pageTitle.Contains("StackDemo"));
+
+                // Click on the Samsung products
+                await page.ClickAsync("//*[@id='__next']/div/div/main/div[1]/div[2]/label/span");
+
+                // Percy Screenshot 1
+                Percy.Snapshot(page, "snapshot_1");
+
+                // Get the text of the current product
+                var productOnPageText = await page.TextContentAsync("//*[@id=\"10\"]/p");
+
+                // Click on 'Add to cart' button
+                await page.ClickAsync("//*[@id=\"10\"]/div[4]");
+
+                // Check if the Cart pane is visible
+                await page.WaitForSelectorAsync("//*[@class=\"float-cart__content\"]");
+
+                // Get the text of the product in the cart
+                var productOnCartText = await page.TextContentAsync("//*[@id='__next']/div/div/div[2]/div[2]/div[2]/div/div[3]/p[1]");
+
+                // Percy Screenshot 2
+                // with options
+                var options = new Dictionary<string, object>
+                {
+                  { "test_case", "should add product to cart" },
+                };
+                Percy.Snapshot(page, "snapshot", options);
+
+                // Assert that the product text matches
+                Assert.AreEqual(productOnCartText, productOnPageText);
+                if (productOnCartText == productOnPageText)
+                {
+                    await MarkTestStatus("passed", "Title matched", page);
+                }
+                else
+                {
+                    await MarkTestStatus("failed", "Tile did not matched", page);
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        [TearDown]
+        public async Task Cleanup()
+        {
+            try
+            {
+                if (page != null)
+                {
+                    await page.CloseAsync();
+                }
+
+                if (context != null)
+                {
+                    await context.CloseAsync();
+                }
+
+                if (browser != null)
+                {
+                    await browser.CloseAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        public static async Task MarkTestStatus(string status, string reason, IPage page) 
+        {
+            Console.WriteLine("Status : " + status + "; Reason : " + reason);
+        }    
+    }
+}
